a5d2bf7f7127f8a272511e5a029554848139f2bd
eniotna05 <antoine.aubay@gmail.com>
2018-01-24 22:00:51 +0100

    parsing done

--- mot_croise.py
+++ mot_croise.py
@@ -1,30 +1,29 @@
-
-
 import time
 import argparse
+import numpy as np
 
 argparser = argparse.ArgumentParser(
     description='Solves crosswords')
 
 argparser.add_argument(
     '-g',
-    '--grille',
-    help='path to grille')
+    '--grid',
+    help='path to grid')
 
 argparser.add_argument(
     '-d',
-    '--dictionnaire',
-    help='path to  dictionnaire')
-
+    '--dictionnary',
+    help='path to dictionnary')
 
 
 class CrossWord:
     def __init__(self,args):
-        self.dic_path = args.dictionnaire
-        self.grille_path = args.grille
+        self.dic_path = args.dictionnary
+        self.grid_path = args.grid
 
-        self.grill_width = None
-        self.grill_length = None
+        self.grid_width = None
+        self.grid_height = None
+        self.grid_array = None
 
 
     def parse(self):
@@ -35,10 +34,18 @@ class CrossWord:
 
         print(word_list)
 
-        with open(self.grill_path, "r") as grill_file:
-            word_list = dict_file.readlines()
-            word_list = [word[:-1] for word in word_list]
+        with open(self.grid_path, "r") as grid_file:
+            grid_lines = grid_file.readlines()
+        grid_lines = [line[:-1] for line in grid_lines]
+        self.grid_width = len(grid_lines[0])
+        self.grid_height = len(grid_lines)
+        self.grid_array = np.zeros((self.grid_width, self.grid_height))
+        for line in range(self.grid_height):
+            for column in range(self.grid_width):
+                if grid_lines[line][column] == ".":
+                    self.grid_array[line][column] = 1
 
+        print(self.grid_array)
 
 if __name__ == '__main__':
     args = argparser.parse_args()
