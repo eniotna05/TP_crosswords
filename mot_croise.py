3a738571418b7b33ecfd06ff4d3e0a375e87e6ef
eymardhouds <32546134+eymardhouds@users.noreply.github.com>
2018-01-30 12:19:33 +0100

    adds

--- mot_croise.py
+++ mot_croise.py
@@ -2,6 +2,8 @@ import time
 import argparse
 import numpy as np
 
+# python mot_croise.py -g crossword1.txt -d words1.txt
+
 argparser = argparse.ArgumentParser(
     description='Solves crosswords')
 
@@ -25,12 +27,15 @@ class CrossWord:
         self.grid_height = None
         self.grid_array = None
 
+        self.word_list=None
+
 
     def parse(self):
 
         with open(self.dic_path, "r") as dict_file:
             word_list = dict_file.readlines()
             word_list = [word[:-1] for word in word_list]
+            self.word_list = word_list
 
         print(word_list)
 
@@ -47,12 +52,42 @@ class CrossWord:
 
         print(self.grid_array)
 
+    def solve(self):
+
+        segments,croisements=get_segments()
+        letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+        words=self.word_list
+
+        # On veut une var du type var = {1: un_mot, 2: un_autre_mot}
+        var = {seg:set(words) for seg in segments}
+
+        # Création du probleme
+        P = constraint_programming(var)
+
+        #Contrainte uniaire: impose la longueur des segments= la longueur d'un mot qui existe (restriction de l'espace)
+        for s in segments:
+            var[str(s)]=set([w for w in words if len(w)==len(s)])
+
+        # Contraintes binaires: pour forcer les mots à s'intersecter correctement selon les croisements que l'on impose
+        BIN = {(i,j) for i in segments for j in segments if i==j}
+
+        # Imaginons par exemple que seg1[i]=seg2[j] (les deux mots se croisent, en l'indice i pour seg1 et j pour seg2)
+        # Il suffit alors de boucler sur les segments en ajoutant les contraintes ainsi de suite puis de faire:
+        P.addConstraint(seg1[i],seg2[j],BIN)
+
+        return P.solve()
+
+    def get_segments(self):
+        """
+        Cette fonction transforme self.words en un tableau de segments [0,0...0] de la taille des mots que l'on cherche
+        Elle retourne aussi là où les mots se croisent (quels indices respectifs)
+
+        """
+        segments=[]
+
+        return segments, croisements
+
 if __name__ == '__main__':
     args = argparser.parse_args()
     solver = CrossWord(args)
     solver.parse()
-
-
-
-
-
